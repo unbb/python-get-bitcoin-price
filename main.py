3d53f0d71974861970892df700928e7393032aeb
unbb <419766923@qq.com>
2023-08-16 13:08:26 +0800

    add new test

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ def main():
             send_email_to_myself(percentage,price) # send email to your own address
         count+=1
         print(count)
-        time.sleep(1800) # get prize every 0.5 hour   
+        time.sleep(1800) # get prize every 0.5 hour 
+        # test  
         
 
-def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+# def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     
 
 if __name__=="__main__":
