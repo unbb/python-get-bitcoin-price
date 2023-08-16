98aeb72c4001bba4f0b5e18f5c5b0f81ee47e250
unbb <419766923@qq.com>
2023-08-16 12:44:36 +0800

    add abs

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
     while(True):
         
         price,percentage = get_percentage_and_price() # get price and percentage of specific coin
-        if percentage>10 or price>2.8: # when the price or percentage is on alert
+        if abs(percentage)>10 or price>2.8: # when the price or percentage is on alert
             send_email_to_myself(percentage,price) # send email to your own address
         count+=1
         print(count)
         time.sleep(1800) # get prize every 0.5 hour   
         
-        
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    
+
 if __name__=="__main__":
     main()
    
